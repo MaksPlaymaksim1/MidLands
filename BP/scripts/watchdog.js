5073d5cb9e121d16d4b30699b1725eaa44d34ec3
ItzSwiftyBoy <74708492+Mizan-Al-Sabbih@users.noreply.github.com>
2023-12-27 11:06:33 +0600

    Delete BP/scripts/watchdog.js

--- BP/scripts/watchdog.js
+++
@@ -1,6 +0,0 @@
-import { system } from "@minecraft/server"
-
-system.events.beforeWatchdogTerminate.subscribe((data) => {
-   data.cancel = true
-   console.warn(`Watchdog tried to crash - ${data.terminateReason}`)
-})
